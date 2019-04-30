226999f072d7e3f6a08bcc1bdffb309baded5c8f
Highdeger <hassan.bhmy@gmail.com>
2019-04-30 13:53:56 +0430

    complete ListViewActivity

--- app/src/main/java/com/example/www/androideverest/ListViewActivity.java
+++ app/src/main/java/com/example/www/androideverest/ListViewActivity.java
@@ -1,12 +1,15 @@
 package com.example.www.androideverest;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,7 +31,7 @@ public class ListViewActivity extends AppCompatActivity {
         names = new ArrayList<>();
 
         for (int i = 1; i < 7; i++) {
-            names.add("Name ===> " + String.valueOf(i));
+            names.add("Name ===> " + i);
         }
 
         adapter = new ArrayAdapter<>(
@@ -39,14 +42,69 @@ public class ListViewActivity extends AppCompatActivity {
         );
 
         myListView.setAdapter(adapter);
+
+        update_list_view_counter();
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selected_item_text = names.get(position);
+                Toast.makeText(ListViewActivity.this, "Selected Item: " + selected_item_text, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        myListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, final View v, int position, long id) {
+                if (v.getVisibility() == View.VISIBLE) {
+                    v.setVisibility(View.INVISIBLE);
+
+                    Runnable runnable = new Runnable() {
+                        @Override
+                        public void run() {
+                            v.setVisibility(View.VISIBLE);
+                        }
+                    };
+                    Handler handler = new Handler();
+
+                    handler.postDelayed(runnable,5000);
+                }
+                return false;
+            }
+        });
+
     }
 
-    public void add_item(View view) {
-//        names.add("Ali ===> " + String.valueOf(counter));
-//        adapter.notifyDataSetChanged();
-
-        adapter.add("Ali ===> " + String.valueOf(counter));
+    public void add_item_first(View view) {
+        names.add(0, "New Item ===> " + counter);
+        adapter.notifyDataSetChanged();
+        update_list_view_counter();
 
         counter++;
+    }
+
+    public void add_item_last(View view) {
+        names.add("New Item ===> " + counter);
+        adapter.notifyDataSetChanged();
+        update_list_view_counter();
+
+//        adapter.add("Ali ===> " + String.valueOf(counter));
+
+        counter++;
+    }
+
+    public void remove_item(View view) {
+        if (!names.isEmpty()) {
+            names.remove(names.size() - 1);
+            adapter.notifyDataSetChanged();
+            update_list_view_counter();
+        } else {
+            Toast.makeText(ListViewActivity.this, "The list is empty already.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void update_list_view_counter() {
+        TextView tv_counter = findViewById(R.id.list_view_counter);
+        tv_counter.setText(String.valueOf(names.size()));
     }
 }
