e08aa13b9f279961421021ae6e57800a437c32a8
Highdeger <hassan.bhmy@gmail.com>
2019-05-29 15:21:15 +0430

    polish NavigationDrawerActivity

--- app/src/main/java/com/example/www/androideverest/NavigationDrawerActivity.java
+++ app/src/main/java/com/example/www/androideverest/NavigationDrawerActivity.java
@@ -6,7 +6,9 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class NavigationDrawerActivity extends AppCompatActivity {
 
@@ -20,13 +22,11 @@ public class NavigationDrawerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_navigation_drawer);
 
         drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
 
         toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open_desc, R.string.navigation_drawer_close_desc);
-
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
-
-        navigationView = findViewById(R.id.navigation_view);
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -35,15 +35,23 @@ public class NavigationDrawerActivity extends AppCompatActivity {
 
                 switch (id) {
                     case R.id.item1:
+                        Toast.makeText(NavigationDrawerActivity.this, "Item 1 Pressed.", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(Gravity.START);
                         break;
 
                     case R.id.item2:
+                        Toast.makeText(NavigationDrawerActivity.this, "Item 2 Pressed.", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(Gravity.START);
                         break;
 
                     case R.id.item3:
+                        Toast.makeText(NavigationDrawerActivity.this, "Item 3 Pressed.", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(Gravity.START);
                         break;
 
                     case R.id.item4:
+                        Toast.makeText(NavigationDrawerActivity.this, "Item 4 Pressed.", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(Gravity.START);
                         break;
                 }
 
